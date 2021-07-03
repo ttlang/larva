f6dc634bf42fccea01fe2cc1f5f682581a3c903b
tyler <lang.tt16@gmail.com>
2021-07-03 10:07:21 +0700

    #0 init project

---
+++ user-auth-service/src/test/java/com/larva/user/auth/UserAuthServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.larva.user.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserAuthServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
